a84e127d98f39a052d46882d4155e587f815c8d0
Anestis Dalgkitsis <anestisdalgkitsis@gmail.com>
2024-05-14 13:37:16 +0200

    Command decoder and router complete.

--- app.py
+++ app.py
@@ -1,13 +1,69 @@
+# Modules
 from flask import Flask, request, jsonify
 from rdflib import Graph
+import networkx as nx
+import datetime
+import socket
+import random
+import time
 
+# Global Settings
+active_partition_algorithm = "default"
+
+# Global Variables
+status = "standby"
+partition_algorithms_list = ["default", "random", "auto"]
+automation_list = ["partition"]
+
+# Module Info
+module_name = "Network Optimization Module"
+module_version = "0.28 Alpha"
+module_ip = socket.gethostbyname(socket.gethostname())
+module_port = "5863"
+module_start_time = time.time()
+module_endpoints = ["management", "service_request"]
+
+# Flask App
 app = Flask(__name__)
 
 # Management API
 
 @app.route('/management', methods=['POST'])
 def management():
-    print("ok")
+
+    incoming_message = request.json
+    command_list = incoming_message.get('command', [])
+
+    # Verbose
+    print("Management -> Incoming message:", incoming_message)
+
+    # Decoder
+    if "algorithm" in command_list:
+        if "partition" in command_list:
+            # Set here algorithm name to default
+            pass
+        pass
+
+    if "list" in command_list:
+        if "automation" in command_list:
+            return jsonify(automation_list)
+        elif "partition" in command_list:
+            return jsonify(partition_algorithms_list)
+
+    if "status" in command_list:
+        report = {
+            "status": status,
+            "name": module_name,
+            "version": module_version,
+            "ip": module_ip,
+            "port": module_port,
+            "uptime": str(datetime.timedelta(seconds=(time.time() - module_start_time))).split('.')[0],
+            "endpoints": module_endpoints,
+            "active partition algorithm": active_partition_algorithm,
+        }
+        return jsonify(report)
+
+    # Return
     return jsonify("ok")
 
 # IBN (RDF) Endpoint
@@ -22,6 +78,11 @@ def process_rdf():
     # Parse RDF file
     g = Graph()
     g.parse(file, format='xml')
+
+    if file:
+        # Send to partition Python script
+        # get back partitioned files
+        pass
 
     # Perform processing (e.g., calculate total weight)
     total_weight = 0
@@ -40,6 +101,8 @@ def process_rdf():
 # Monitoring Endpoint
 # << TBD >>
 
+# Functions
+
 # MAIN
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=module_port)
--- management.py
+++ management.py
@@ -1,18 +1,19 @@
 import requests
 
 def send_command(command):
-    url = 'http://127.0.0.1:5000/management'
+    url = 'http://127.0.0.1:5863/management'
     data = {'command': command}
     response = requests.post(url, json=data)
     
-    print("Response Status Code:", response.status_code)
-    print("Response Content:", response.content)
+    # Verbose
+    # print("Response Status Code:", response.status_code)
+    # print("Response Content:", response.content)
 
     return response.json()
 
 def help_message():
     print("Available Commands:")
-    print("- status: Report")
+    print("- status: General module report.")
     print("- algorithm [automation] [auto/default/random]: Pick an algorithm for automation.")
     print("- list [automation]: shows all available algorithms")
     print("- help [command]: Additional help for a command.")
@@ -22,7 +23,7 @@ if __name__ == '__main__':
 
     # Welcome text
     print("Network Optimizer Client | Desire6G")
-    print("Enter command and press the Return key to execute, type help for a list of commands")
+    print("Enter command and press the Return key to execute, type help for a list of commands\n")
 
     # Loop
     while True:
@@ -36,21 +37,18 @@ if __name__ == '__main__':
         if command[0] == "help":
             help_message()
         elif command[0] == "status":
-            if len(command) is not 1:
-                result = send_command(command[1])
-                print("Server response:", result)
-            else:
-                print("Second argument missing.")
+            result = send_command(command)
+            print("Response:", result)
         elif command[0] == "algorithm":
-            if len(command) is not 1:
-                result = send_command(command[1])
-                print("Server response:", result)
+            if len(command) != 2:
+                result = send_command(command)
+                print("Response:", result)
             else:
-                print("Second argument missing.")
+                print("Missing arguments, type help.")
         elif command[0] == "list":
-            if len(command) is not 1:
-                result = send_command(command[1])
-                print("Server response:", result)
+            if len(command) != 1:
+                result = send_command(command)
+                print("Response:", result)
             else:
                 print("Second argument missing.")
         elif command[0] == "exit":
